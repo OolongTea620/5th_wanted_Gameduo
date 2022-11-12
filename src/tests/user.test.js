bcc53ff38cb9f012862f7719683a8f8263de5c72
OolongTea620 <rlafls9596@gmail.com>
2022-11-12 17:57:44 +0900

    test: userId 생성 확인

---
+++ src/tests/user.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const { createApp } = require("../../app");
+const { sequelize } = require("../models");
+
+beforeAll(async () => {
+  app = createApp();
+  await sequelize.sync();
+});
+
+afterAll(async () => {
+  await sequelize.query(`SET foreign_key_checks = 0`);
+  await sequelize.query(`TRUNCATE user`);
+  await sequelize.query(`SET foreign_key_checks = 1`);
+  await sequelize.close();
+});
+
+describe("POST /user", () => {
+  test("create user : success", async () => {
+    const response = await request(app).post("/user");
+    console.log(response.body);
+    expect(response.status).toEqual(201);
+    expect(!!response.body.userId).toEqual(true);
+  });
+});
