01007eeb877454f706a0e238ca70a6cf028895a8
OolongTea620 <rlafls9596@gmail.com>
2022-11-12 21:50:56 +0900

    init : redis 연결

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ require("dotenv").config();
 
 const { createApp } = require("./app");
 const { sequelize } = require("./src/models");
+const redis = require("redis");
+
 const startServer = async () => {
   const app = createApp();
   const PORT = process.env.PORT;
+  const redisClient = redis.createClient({ legacyMode: true });
 
   await sequelize
-    .sync({ force: false, alter: true })
+    .sync({ force: false, alter: false })
     .then(() => {
-      console.log("연결 성공");
+      console.log("mysql 연결 성공");
     })
     .catch((err) => {
       console.error(err);
@@ -18,6 +21,11 @@ const startServer = async () => {
   app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
   });
+
+  redisClient.on("connect", () => console.log("Redis Connect success"));
+  redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+  await redisClient.connect();
 };
 
 startServer();
