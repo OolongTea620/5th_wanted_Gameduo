749956353dce627d5995f6395d09606984b9b3cb
OolongTea620 <rlafls9596@gmail.com>
2022-12-20 20:17:13 +0900

    fix: delete sequelize, lru-cahe, init redis

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 require("dotenv").config();
 
 const { createApp } = require("./app");
-const { sequelize } = require("./src/models");
-const putScoreInfoCache = require("./src/cache/getScoreInfo");
 
 const startServer = async () => {
   const app = createApp();
   const PORT = process.env.PORT;
 
-  await sequelize
-    .sync({ force: false, alter: true })
-    .then(() => {
-      console.log("mysql 연결 성공");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-
   app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
   });
-
-  await putScoreInfoCache();
 };
 
 startServer();
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const errorHandler = require("../middlewares/errorHandler");
 const userController = require("../controllers/userController");
 
-router.get("/:userId", errorHandler(userController.getUserInfo));
+// router.get("/:userId", errorHandler(userController.getUserInfo));
 
-router.post("", errorHandler(userController.signup));
+// router.post("", errorHandler(userController.signup));
 
 module.exports = router;
